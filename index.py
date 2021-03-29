06d8ee3789451c84144c8d7e355928bdcdd63c9e
Marcos Borges <marcos.lima@simpleagro.com.br>
2021-03-29 16:28:10 +0000

    add predict pages

--- index.py
+++ index.py
@@ -5,6 +5,8 @@ if APP_DIR:
 
 from eda_basketball import basketball_app
 from eda_cryptocurrency import crypto_price_app
+from classification_iris import iris_ml_app
+from regression_boston_housin import boston_house_ml_app
 from eda_football import football_app
 from eda_sp_500 import sp_500
 import streamlit as st
@@ -24,6 +26,9 @@ app.add_app("(NBA)Baketball Data", basketball_app.view)
 app.add_app("Crypto Price Data", crypto_price_app.view)
 app.add_app("(NFL)Fotball Data", football_app.view)
 app.add_app("S&P500 Companyes Price", sp_500.view)
+app.add_app("Simple Iris Flower Prediction", iris_ml_app.view)
+app.add_app("Boston House Price Prediction App", boston_house_ml_app.view)
+
 
 
 app.run()
