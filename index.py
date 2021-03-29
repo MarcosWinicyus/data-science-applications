903e296d2bd95663d9047f2275703747e4208a3d
Marcos Borges <marcos.lima@simpleagro.com.br>
2021-03-29 04:04:11 +0000

    add cpytho app and index init

---
+++ index.py
@@ -0,0 +1,27 @@
+import os
+os.chdir("/personal/study/streamlit")
+
+from eda_basketball import basketball_app
+from eda_cryptocurrency import crypto_price_app
+from eda_football import football_app
+from eda_sp_500 import sp_500
+import streamlit as st
+from multiapp import MultiApp
+
+# page expands to full width
+# st.set_page_config(
+#         page_title="Data Science APP`S",
+#         page_icon="🧊",
+#         layout="wide",
+#         initial_sidebar_state="expanded"
+#     )
+
+app = MultiApp()
+
+app.add_app("(NBA)Baketball Data", basketball_app.view)
+app.add_app("Crypto Price Data", crypto_price_app.view)
+app.add_app("(NFL)Fotball Data", football_app.view)
+app.add_app("S&P500 Companyes Price", crypto_price_app.view)
+
+
+app.run()
